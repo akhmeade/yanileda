b86d6971d7c30cb159e332da9f1d47099c70092f
nma2207 <nma2207@gmail.com>
2020-04-07 21:05:50 +0300

    add decoration to avoid too many requests error

--- model/bublic_yadisk_model.py
+++ model/bublic_yadisk_model.py
@@ -5,28 +5,30 @@ from yadisk.exceptions import TooManyRequestsError
 
 import magic_const
 
+def avoid_too_many_requests_error(fn):
+    def wrapped(*args):
+        result = None
+        while result is None:
+            try:
+                #print(args)
+                result = fn(*args)
+            except TooManyRequestsError:
+                print('too many')
+                result = None
+        return result
+    return wrapped
+
 class BublicYadiskModel:
     header = ("Type", "Name", "Public Url", "Last modified")
     def __init__(self):
         self.disk = yadisk.YaDisk()
     
+    @avoid_too_many_requests_error
     def check_url(self, url):
         print(url)
-        result = None
-        while result is None:
-
-            try:
-                is_correct = self.disk.public_exists(url)
-                print(True)
-                result = is_correct
-            except TooManyRequestsError:
-                print("too many")
-                result = None
-            except Exception as e:
-                print(False)
-                result = False
+        result = self.disk.public_exists(url)
         return result
-    
+    @avoid_too_many_requests_error
     def get_meta(self, url):
         print(url)
         info = self.disk.get_public_meta(url)
@@ -34,6 +36,7 @@ class BublicYadiskModel:
             (info.type, info.name, info.public_url,
                 info.created.strftime(magic_const.DATETIME_FORMAT))], url
     
+    @avoid_too_many_requests_error
     def get_listdir(self, url):
         if self.check_url(url):
             if self.disk.is_public_dir(url):
@@ -55,7 +58,7 @@ class BublicYadiskModel:
         #             (i.name, i.created.strftime(magic_const.DATETIME_FORMAT)))
         # else:
         #     file = self.disk.
-
+    @avoid_too_many_requests_error
     def download(self, from_path, to_path):
         if self.check_url(from_path):
             self.disk.download_public(from_path, to_path)
