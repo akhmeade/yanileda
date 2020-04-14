0c3a11a96ae82def8e0d611dbbf9528e3ef81d98
nma2207 <nma2207@gmail.com>
2020-04-14 07:51:53 +0300

    Add Result class to control Messages

--- utils.py
+++ utils.py
@@ -12,3 +12,33 @@ def is_url(url):
         return all([result.scheme, result.netloc])
     except ValueError:
         return False
+
+class Result:
+    def __init__(self):
+        self._is_ok = False
+        self._result = None
+        self._error_message = ""
+    
+    @staticmethod
+    def success(result):
+        obj = Result()
+        obj._result = result
+        obj._is_ok = True
+        obj._error_message = ""
+        return obj
+    
+    @staticmethod
+    def failed(error_message):
+        result = Result()
+        result._is_ok = False
+        result._error_message = error_message
+        return result
+    
+    def result(self):
+        return self._result
+    
+    def error_message(self):
+        return self._error_message
+    
+    def is_ok(self):
+        return self._is_ok
